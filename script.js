79b5665fac3add4df0952bb672f06444254a04b6
kcho <hellokcho@gmail.com>
2015-08-02 13:57:18 +0000

    updated

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-	localStorage.removeItem("udmacau");
-	localStorage.removeItem("udmacau_e");
+localStorage.removeItem("udmacau");
+localStorage.removeItem("udmacau_e");
+
+localStorage.setItem("DATA_e_r1Ax",'##Since 4/Jun/2015<br>"Estrada do Arco" stop is suspended, please use "Istmo F. Amaral/Triangle Garden" stop.');localStorage.setItem("DATA_r1Ax",'##2015年6月4日起<br>不再行經「拱形馬路」站，請使用「關閘馬路/三角花園」站上落。');
+localStorage.setItem("DATA_e_r7y",'##Since 4/Jun/2015<br>"Estrada do Arco" stop is suspended, please use "Istmo F. Amaral/Triangle Garden" stop.');localStorage.setItem("DATA_r7y",'##2015年6月4日起<br>不再行經「拱形馬路」站，請使用「關閘馬路/三角花園」站上落。');
+localStorage.setItem("DATA_e_r7Ay",'##Since 4/Jun/2015<br>"Estrada do Arco" stop is suspended, please use "Istmo F. Amaral/Triangle Garden" stop.');localStorage.setItem("DATA_r7Ay",'##2015年6月4日起<br>不再行經「拱形馬路」站，請使用「關閘馬路/三角花園」站上落。');
+localStorage.setItem("DATA_e_r28Bx",'##Since 4/Jun/2015<br>"Estrada do Arco" stop is suspended, please use "Istmo F. Amaral/Triangle Garden" stop.');localStorage.setItem("DATA_r28Bx",'##2015年6月4日起<br>不再行經「拱形馬路」站，請使用「關閘馬路/三角花園」站上落。');
+localStorage.setItem("DATA_e_r28BXx",'##Since 4/Jun/2015<br>"Estrada do Arco" stop is suspended, please use "Istmo F. Amaral/Triangle Garden" stop.');localStorage.setItem("DATA_r28BXx",'##2015年6月4日起<br>不再行經「拱形馬路」站，請使用「關閘馬路/三角花園」站上落。');
+localStorage.setItem("DATA_e_M27",'##Since 4/Jun/2015<br>Rt 1A, 28B, 28BX for Ferry Terminal and Rt 7, 7A for Iao Hon stop at this stop.');localStorage.setItem("DATA_M27",'##2015年6月4日起<br>往港澳碼頭方向的1A、28B、28BX及往祐漢的7、7A路線停靠本站。');
+localStorage.setItem("DATA_e_M9",'##Since 4/Jun/2015<br>This stop is suspended, please use "Istmo F. Amaral/Triangle Garden" stop.');localStorage.setItem("DATA_M9",'##2015年6月4日起<br>本站停用，請使用「關閘馬路/三角花園」站上落。');
