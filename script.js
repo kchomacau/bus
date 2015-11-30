560fedd1bb600323da3669e92ddd0cdd35c9e147
kcho <hellokcho@gmail.com>
2015-11-30 14:59:19 +0000

    updated

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ passedt();
 localStorage.removeItem("udmacau");
 localStorage.removeItem("udmacau_e");
 
-if((vers!="v65")){
+if((vers!="v65")||(vers!="v66")){
     var link = '<a ontouchstart="localStorage.setItem(\'getgetmacau\',\'\');localStorage.setItem(\'udmacau\',\'\');localStorage.setItem(\'udmacau_e\',\'\');window.location.href=\'https://play.google.com/store/apps/details?id=com.locmacau&apk\'" class="upd-icon">';
 
     localStorage.setItem("udmacau",link+"<b>澳門出行易 V4.2.4</b><u>更新路線資訊</u><div>檢視</div></a>");
