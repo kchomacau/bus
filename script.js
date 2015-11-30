448eab1bfad486f31b302d231c3b781fda91c213
kcho <hellokcho@gmail.com>
2015-11-30 15:27:42 +0000

    updated

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ passedt();
 localStorage.removeItem("udmacau");
 localStorage.removeItem("udmacau_e");
 
-if((vers!="v65")||(vers!="v66")){
+if((vers!="v66")){
     var link = '<a ontouchstart="localStorage.setItem(\'getgetmacau\',\'\');localStorage.setItem(\'udmacau\',\'\');localStorage.setItem(\'udmacau_e\',\'\');window.location.href=\'https://play.google.com/store/apps/details?id=com.locmacau&apk\'" class="upd-icon">';
 
-    localStorage.setItem("udmacau",link+"<b>澳門出行易 V4.2.4</b><u>更新路線資訊</u><div>檢視</div></a>");
-    localStorage.setItem("udmacau_e",link+"<b>Macau EasyGo V4.2.4</b><u>Update Route Info</u><div>VIEW</div></a>");
+    localStorage.setItem("udmacau",link+"<b>澳門出行易 V4.2.5</b><u>首頁顯示當前位置</u><div>檢視</div></a>");
+    localStorage.setItem("udmacau_e",link+"<b>Macau EasyGo V4.2.5</b><u>Display Your Location On Homepage</u><div>VIEW</div></a>");
     window.location.reload();
 }
