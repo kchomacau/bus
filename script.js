324358d1511ac4fa8f013024bf6014da6e33d39a
kcho <hellokcho@gmail.com>
2015-12-18 05:52:40 +0000

    updated

--- script.js
+++ script.js
@@ -9,23 +9,23 @@ localStorage.setItem("DATA_r2x",'##2015年11月25日起<br>往媽閣方向臨時
 localStorage.setItem("DATA_e_r30x",'##2015/Dec/14 - 2016/Feb/11<br>Bus stop "Av. Kwong Tung/Jockey Club" is suspended temporarily.');
 localStorage.setItem("DATA_r30x",'##2015年12月14日至2016年2月11日<br>不停靠「廣東大馬路/馬會」站');
 
-localStorage.setItem("DATA_e_r6Bx",'##2015/Dec/17-31<br>Bus stop "Yuet Wah College" is suspended for RIVIERA MACAU bound temporarily. Passengers may use "Tap Seac Multi-sports Pavilion" stop instead.');
-localStorage.setItem("DATA_r6Bx",'##2015年12月17日至31日<br>往凱泉灣方向不停靠「粵華中學」站，請使用「塔石體育館」站上落。');
+localStorage.setItem("DATA_e_r6Bx",'##2015/Dec/17-31<br>Bus stop "Yuet Wah College" is suspended for RIVIERA MACAU bound temporarily. Passengers may use "Tap Seac Multi-sports Pavilion" (Beside Tap Seac Plaza) stop instead.');
+localStorage.setItem("DATA_r6Bx",'##2015年12月17日至31日<br>往凱泉灣方向不停靠「粵華中學」站，請使用「塔石體育館」 （舊愛都酒店）站上落。');
 
-localStorage.setItem("DATA_e_r17x",'##2015/Dec/17-31<br>Bus stops "Chan Sui Ki Perpetual Help College" and "Yuet Wah College" are suspended for CENTRO CULTURAL bound temporarily. Passengers may use "Victory Garden" or "Tap Seac Multi-sports Pavilion" stop instead.');
-localStorage.setItem("DATA_r17x",'##2015年12月17日至31日<br>往文化中心方向不停靠「陳瑞祺中學」、「粵華中學」站，請使用「得勝花園」或「塔石體育館」站上落。');
+localStorage.setItem("DATA_e_r17x",'##2015/Dec/17-31<br>Bus stops "Chan Sui Ki Perpetual Help College" and "Yuet Wah College" are suspended for CENTRO CULTURAL bound temporarily. Passengers may use "Victory Garden" or "Tap Seac Multi-sports Pavilion" (Beside Tap Seac Plaza) stop instead.');
+localStorage.setItem("DATA_r17x",'##2015年12月17日至31日<br>往文化中心方向不停靠「陳瑞祺中學」、「粵華中學」站，請使用「得勝花園」或「塔石體育館」（舊愛都酒店）站上落。');
 
-localStorage.setItem("DATA_e_r28Cx",'##2015/Dec/17-31<br>Bus stops "Chan Sui Ki Perpetual Help College" and "Yuet Wah College" are suspended for JAI ALAI bound temporarily. Passengers may use "Victory Garden" or "Tap Seac Multi-sports Pavilion" stop instead.');
-localStorage.setItem("DATA_r28Cx",'##2015年12月17日至31日<br>往回力方向不停靠「陳瑞祺中學」、「粵華中學」站，請使用「得勝花園」或「塔石體育館」站上落。');
+localStorage.setItem("DATA_e_r28Cx",'##2015/Dec/17-31<br>Bus stops "Chan Sui Ki Perpetual Help College" and "Yuet Wah College" are suspended for JAI ALAI bound temporarily. Passengers may use "Victory Garden" or "Tap Seac Multi-sports Pavilion" (Beside Tap Seac Plaza) stop instead.');
+localStorage.setItem("DATA_r28Cx",'##2015年12月17日至31日<br>往回力方向不停靠「陳瑞祺中學」、「粵華中學」站，請使用「得勝花園」或「塔石體育館」（舊愛都酒店）站上落。');
 
 localStorage.setItem("DATA_e_M72",'##2015/Dec/17-31<br>This stop is suspended temporarily. Please use "Victory Garden" stop instead.');
 localStorage.setItem("DATA_M72",'##2015年12月17日至31日<br>本站停用，請使用「得勝花園」站上落。');
 
-localStorage.setItem("DATA_e_M149",'##2015/Dec/17-31<br>This stop is suspended temporarily. Please use "Tap Seac Multi-sports Pavilion" stop instead.');
-localStorage.setItem("DATA_M149",'##2015年12月17日至31日<br>本站停用，請使用「塔石體育館」站上落。');
+localStorage.setItem("DATA_e_M149",'##2015/Dec/17-31<br>This stop is suspended temporarily. Please use "Tap Seac Multi-sports Pavilion" (Beside Tap Seac Plaza) stop instead.');
+localStorage.setItem("DATA_M149",'##2015年12月17日至31日<br>本站停用，請使用「塔石體育館」（舊愛都酒店）站上落。');
 
-localStorage.setItem("DATA_e_r6Ax",'##Since 2015/Nov/25<br>Bus stop "Venceslau Morais/Airway Bldg." is added for HOSPITAL S. JANUÁRIO bound temporarily.<br>&nbsp;<br>2015/Dec/17-31<br>Bus stop "Yuet Wah College" is suspended for P. SERENIDADE bound temporarily. Passengers may use "Tap Seac Multi-sports Pavilion" stop instead.');
-localStorage.setItem("DATA_r6Ax",'##2015年11月25日起<br>往山頂醫院方向臨時停靠「慕拉士/飛通大廈」站<br>&nbsp;<br>2015年12月17日至31日<br>往順景廣場方向不停靠「粵華中學」站，請使用「塔石體育館」站上落。');
+localStorage.setItem("DATA_e_r6Ax",'##Since 2015/Nov/25<br>Bus stop "Venceslau Morais/Airway Bldg." is added for HOSPITAL S. JANUÁRIO bound temporarily.<br>&nbsp;<br>2015/Dec/17-31<br>Bus stop "Yuet Wah College" is suspended for P. SERENIDADE bound temporarily. Passengers may use "Tap Seac Multi-sports Pavilion" (Beside Tap Seac Plaza) stop instead.');
+localStorage.setItem("DATA_r6Ax",'##2015年11月25日起<br>往山頂醫院方向臨時停靠「慕拉士/飛通大廈」站<br>&nbsp;<br>2015年12月17日至31日<br>往順景廣場方向不停靠「粵華中學」站，請使用「塔石體育館」（舊愛都酒店）站上落。');
 
 
 
