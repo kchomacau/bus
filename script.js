c4d9187d4379e30c146f746d2421d91d44160e92
kcho <hellokcho@gmail.com>
2016-02-17 13:29:51 +0000

    updated

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ passedt();
 localStorage.removeItem("udmacau");
 localStorage.removeItem("udmacau_e");
 
-localStorage.setItem("DATA_e_r30x",'##2015/Dec/14 - 2016/Feb/11<br>Bus stop "Av. Kwong Tung/Jockey Club" is skipped.');localStorage.setItem("DATA_r30x",'##2015年12月14日至2016年2月11日<br>不停靠「廣東大馬路/馬會」站。');
 localStorage.setItem("DATA_e_r3Xx",'##From now on, <br>Route 3X is also available in 20H00-23H00, buses depart every 6-10 min.');localStorage.setItem("DATA_r3Xx",'##即日起，<br>本線新增晚上8時至11時的服務，約6至10分鐘開出一班車。');
-
-localStorage.setItem("DATA_e_r5Xx",'##Correction:<br>Route 5X runs between Fai Chi Kei and <del>Hospital S. Januario</del> N.A.P.E.');
-
+localStorage.setItem("DATA_e_r21Ax",'##From now on, <br>Route 21A goes to Ka-Ho Fuel Oil Terminal after arriving Hac-Sa Beach. Service Hour from 06H30 to 23H30, departs every 30 mins.');localStorage.setItem("DATA_r21Ax",'##即日起，<br>本線停黑沙海灘站後繼續前往九澳油庫，服務時間改為06H30-23H30，每半小時一班');
+localStorage.setItem("DATA_e_r21Ay",'##From now on, <br>Route 21A departs every 30 mins from Ka-Ho Fuel Oil Terminal, via Hac-Sa Village, then goes back to Hac-Sa Beach and follows the original route until Barra Terminus. Service Hour from 07H20 to 00H20.');localStorage.setItem("DATA_r21Ay",'##即日起，<br>本線服務時間改為07H20-00H20，每半小時一班由九澳油庫開出，經黑沙村至本站後，按原線前往媽閣。');
 
 if((vers!="v68")){
     var link = '<a ontouchstart="localStorage.setItem(\'getgetmacau\',\'\');localStorage.setItem(\'udmacau\',\'\');localStorage.setItem(\'udmacau_e\',\'\');window.location.href=\'https://play.google.com/store/apps/details?id=com.locmacau&apk\'" class="upd-icon">';
