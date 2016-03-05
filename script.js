e56a124d7fc4d66a2fa01dd801166d2641e3a332
kcho <hellokcho@gmail.com>
2016-03-05 04:50:21 +0000

    updated

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ passedt();
 localStorage.removeItem("udmacau");
 localStorage.removeItem("udmacau_e");
 
-localStorage.setItem("DATA_e_r3Xx",'##From now on, <br>Route 3X is also available in 20H00-23H00, buses depart every 6-10 min.');localStorage.setItem("DATA_r3Xx",'##即日起，<br>本線新增晚上8時至11時的服務，約6至10分鐘開出一班車。');
-localStorage.setItem("DATA_e_r21Ax",'##From now on, <br>Route 21A goes to Ka-Ho Fuel Oil Terminal after arriving Hac-Sa Beach. Service Hour from 06H30 to 23H30, departs every 30 mins.');localStorage.setItem("DATA_r21Ax",'##即日起，<br>本線停黑沙海灘站後繼續前往九澳油庫，服務時間改為06H30-23H30，每半小時一班');
-localStorage.setItem("DATA_e_r21Ay",'##From now on, <br>Route 21A departs every 30 mins from Ka-Ho Fuel Oil Terminal, via Hac-Sa Village, then goes back to Hac-Sa Beach and follows the original route until Barra Terminus. Service Hour from 07H20 to 00H20.');localStorage.setItem("DATA_r21Ay",'##即日起，<br>本線服務時間改為07H20-00H20，每半小時一班由九澳油庫開出，經黑沙村至本站後，按原線前往媽閣。');
+localStorage.setItem("DATA_e_r3Xx",'##From 22/Jan/2016<br>Route 3X is also available in 20H00-23H00, buses depart every 6-10 min.');localStorage.setItem("DATA_r3Xx",'##2016年1月22日起<br>本線新增晚上8時至11時的服務，約6至10分鐘開出一班車。');
+localStorage.setItem("DATA_e_r21Ax",'##From 3/Feb/2016<br>This route goes to Ka-Ho Fuel Oil Terminal after arriving at Hac-Sa Beach. Service Hour is 06H30 to 23H30, buses departs every 30 mins.');localStorage.setItem("DATA_r21Ax",'##2016年2月3日起<br>本線停黑沙海灘站後繼續前往九澳油庫，服務時間改為06H30-23H30，每半小時一班');
+localStorage.setItem("DATA_e_r21Ay",'##From 3/Feb/2016<br>This route departs every 30 mins from Ka-Ho Fuel Oil Terminal, then back to original route at Hac-Sa Beach. Service Hour is 07H20 to 00H20.');localStorage.setItem("DATA_r21Ay",'##2016年2月3日起<br>本線服務時間改為07H20-00H20，每半小時一班由九澳油庫開出，經黑沙村至黑沙海灘站後，按原線前往媽閣。');
+localStorage.setItem("DATA_e_C669",'##From 3/Feb/2016<br>Route 21A goes to T. DE COMBUSTÍVEIS DO P. DE KÁ-HÓ after arriving at this stop. That means, route 21A stops at this stop on both trip. Please pay attention to the direction.');localStorage.setItem("DATA_C669",'##2016年2月3日起<br>路線21A延伸至九澳油庫，來回程均經本站，上車前請看清方向。');
+localStorage.setItem("DATA_e_r34y",'##From 7/Mar/2016<br>This route skip "RUA DE AVEIRO", "ESCOLA HOU KONG DA TAIPA" and "RUA ÉVORA/FLOWER CITY" stop. You may use "Va Pou Garden Bldg." or "Garden of Flower City" stop instead.');localStorage.setItem("DATA_r34y",'##2016年3月7日起<br>本線不停靠「亞威羅街」、「氹仔濠江中學」及「埃武拉街/花城」站，您可使用「華寶花園」或「花城公園」站上落。');
+localStorage.setItem("DATA_e_T348",'##From 7/Mar/2016<br>This stop is temporary suspended. You may use "Va Pou Garden Bldg." or "Garden of Flower City" stop instead.');localStorage.setItem("DATA_T348",'##2016年3月7日起<br>本站停用，您可使用「華寶花園」或「花城公園」站上落。');
+localStorage.setItem("DATA_e_T349",'##From 7/Mar/2016<br>This stop is temporary suspended. You may use "Kingsville" stop instead.');localStorage.setItem("DATA_T349",'##2016年3月7日起<br>本站停用，您可使用「雍景灣」站上落。');
+localStorage.setItem("DATA_e_T340",'##From 7/Mar/2016<br>Route 34 skips this stop. You may use "Nam Sun Garden Bldg." stop instead.');localStorage.setItem("DATA_T340",'##2016年3月7日起<br>路線34不停靠本站，您可使用「南新花園」站上落。');
 
 if((vers!="v68")){
     var link = '<a ontouchstart="localStorage.setItem(\'getgetmacau\',\'\');localStorage.setItem(\'udmacau\',\'\');localStorage.setItem(\'udmacau_e\',\'\');window.location.href=\'https://play.google.com/store/apps/details?id=com.locmacau&apk\'" class="upd-icon">';
@@ -14,4 +19,3 @@ if((vers!="v68")){
     localStorage.setItem("udmacau_e",link+"<b>Macau EasyGo V4.3</b><u>Update 24 Routes</u><div>VIEW</div></a>");
     window.location.reload();
 }
-
