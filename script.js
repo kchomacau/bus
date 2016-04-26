9298e9db9de950cb8b3b214a0222692a13d79a97
kcho <hellokcho@gmail.com>
2016-04-26 15:45:44 +0000

    updated

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ localStorage.setItem("DATA_e_T348",'##From 7/Mar/2016<br>This stop is temporary 
 localStorage.setItem("DATA_e_T349",'##From 7/Mar/2016<br>This stop is temporary suspended. You may use "Kingsville" stop instead.');localStorage.setItem("DATA_T349",'##2016年3月7日起<br>本站停用，您可使用「雍景灣」站上落。');
 localStorage.setItem("DATA_e_T340",'##From 7/Mar/2016<br>Route 34 skips this stop. You may use "Nam Sun Garden Bldg." stop instead.');localStorage.setItem("DATA_T340",'##2016年3月7日起<br>路線34不停靠本站，您可使用「南新花園」站上落。');
 localStorage.setItem("DATA_e_r18y",'##15/Apr - 4/May/2016<br>"Rua das Estalagens" and "R. Cinco Outubro" stop are suspended. You may use the temp. stop located at the intersection of Rua dos Faitiões and Rua da Tercena.');localStorage.setItem("DATA_r18y",'##2016年4月15日至5月4日<br>「草堆街」站及「十月初五街」站停用，您可使用快艇頭街與果欄街交界的臨時站上落。');
+localStorage.setItem("DATA_e_r4x",'##From 27/Apr/2016<br>"Van Sion Son Chun" temp. stop is added after "Lam Mau/Macau Yacht Club".');localStorage.setItem("DATA_r4x",'##2016年4月27日起<br>本線在停靠「林茂/澳門遊艇會」站後，新增停靠「運順新邨」臨時站，您可使用該站上落。');
 
 if((vers!="v70")){
     var link = '<a ontouchstart="localStorage.setItem(\'getgetmacau\',\'\');localStorage.setItem(\'udmacau\',\'\');localStorage.setItem(\'udmacau_e\',\'\');window.location.href=\'https://play.google.com/store/apps/details?id=com.locmacau&apk\'" class="upd-icon">';
