3fe93f2c5fb6369848708d9df2dc043250e9981a
kcho <hellokcho@gmail.com>
2016-04-30 14:22:28 +0000

    updated

--- script.js
+++ script.js
@@ -3,12 +3,37 @@ passedt();
 localStorage.removeItem("udmacau");
 localStorage.removeItem("udmacau_e");
 
-localStorage.setItem("DATA_e_r34y",'##From 7/Mar/2016<br>This route skips "Rua de Aveiro", "Hou Kong Middle School Taipa" and "Rua Evora/Flower City" stop. You may use "Nam Sun Garden Bldg.", "Va Pou Garden Bldg." or "Garden of Flower City" stop instead.');localStorage.setItem("DATA_r34y",'##2016年3月7日起<br>本線不停靠「亞威羅街」、「氹仔濠江中學」及「埃武拉街/花城」站，您可使用「南新花園」、「華寶花園」或「花城公園」站上落。');
-localStorage.setItem("DATA_e_T348",'##From 7/Mar/2016<br>This stop is temporary suspended. You may use "Va Pou Garden Bldg." or "Garden of Flower City" stop instead.');localStorage.setItem("DATA_T348",'##2016年3月7日起<br>本站停用，您可使用「華寶花園」或「花城公園」站上落。');
-localStorage.setItem("DATA_e_T349",'##From 7/Mar/2016<br>This stop is temporary suspended. You may use "Kingsville" stop instead.');localStorage.setItem("DATA_T349",'##2016年3月7日起<br>本站停用，您可使用「雍景灣」站上落。');
-localStorage.setItem("DATA_e_T340",'##From 7/Mar/2016<br>Route 34 skips this stop. You may use "Nam Sun Garden Bldg." stop instead.');localStorage.setItem("DATA_T340",'##2016年3月7日起<br>路線34不停靠本站，您可使用「南新花園」站上落。');
+
+localStorage.setItem("DATA_e_r34y",'##From now on,<br>Rt 34 skips "Rua de Aveiro", "Hou Kong Middle School Taipa" and "Rua Evora/Flower City" stop. You may use "Nam Sun Garden Bldg.", "Va Pou Garden Bldg." or "Garden of Flower City" stop instead.');localStorage.setItem("DATA_r34y",'##即日起<br>本線不停靠「亞威羅街」、「氹仔濠江中學」及「埃武拉街/花城」站，您可使用「南新花園」、「華寶花園」或「花城公園」站上落。');
+localStorage.setItem("DATA_e_T348",'##From now on,<br>This stop is cancelled. You may use "Va Pou Garden Bldg." or "Garden of Flower City" stop instead.');localStorage.setItem("DATA_T348",'##即日起<br>本站停用，您可使用「華寶花園」或「花城公園」站上落。');
+localStorage.setItem("DATA_e_T349",'##From now on,<br>This stop is cancelled. You may use "Kingsville" stop instead.');localStorage.setItem("DATA_T349",'##即日起<br>本站停用，您可使用「雍景灣」站上落。');
+localStorage.setItem("DATA_e_T340",'##From now on,<br>Rt 34 skips this stop. You may use "Nam Sun Garden Bldg." stop instead.');localStorage.setItem("DATA_T340",'##即日起<br>路線34不停靠本站，您可使用「南新花園」站上落。');
 localStorage.setItem("DATA_e_r18y",'##15/Apr - 4/May/2016<br>"Rua das Estalagens" and "R. Cinco Outubro" stop are suspended. You may use the temp. stop located at the intersection of Rua dos Faitiões and Rua da Tercena.');localStorage.setItem("DATA_r18y",'##2016年4月15日至5月4日<br>「草堆街」站及「十月初五街」站停用，您可使用快艇頭街與果欄街交界的臨時站上落。');
-localStorage.setItem("DATA_e_r4x",'##From 27/Apr/2016<br>"Van Sion Son Chun" temp. stop is added after "Lam Mau/Macau Yacht Club".');localStorage.setItem("DATA_r4x",'##2016年4月27日起<br>本線在停靠「林茂/澳門遊艇會」站後，新增停靠「運順新邨」臨時站，您可使用該站上落。');
+localStorage.setItem("DATA_e_r4x",'##From now on,<br>"Van Sion Son Chun" temp. stop is added after "Lam Mau/Macau Yacht Club".');localStorage.setItem("DATA_r4x",'##即日起<br>本線在停靠「林茂/澳門遊艇會」站後，新增停靠「運順新邨」臨時站，您可使用該站上落。');
+localStorage.setItem("DATA_e_M184",'##From now on,<br>This stop is moved forwards 100m, opposites to McDonald\'s.');localStorage.setItem("DATA_M184",'##即日起<br>本站往媽閣方向遷移至小販巷口。');
+localStorage.setItem("DATA_e_r39x",'##From now on,<br>"Rotunda Dr. Carlos A. C. P. D'Assumpcao", "Esparteiro/Lou Lim Ieok" stop are added before "Praca Ferreira Amaral". After that, "Esparteiro/Regency", "Av. Dr. Sun Yat Sen/R. Lagos", "Chun Luen Garden Bldg" stop are added before going back to "Edf. do Lago" terminus.');localStorage.setItem("DATA_r39x",'##即日起<br>本線往亞馬喇前地方向，先停靠「宋玉生博士圓形地」及「史伯泰/盧廉若」站，再停靠亞馬喇前地；往湖畔大廈方向，先停靠「史伯泰/麗景灣」、「氹仔大中華廣場」「泉亮花園」再返回總站。');
+localStorage.setItem("DATA_e_T307",'##From now on,<br>This stop is added to Rt 39 (Praca Ferreira Amaral bound)');localStorage.setItem("DATA_T307",'##即日起<br>路線39前往亞馬喇前地方向新增停靠本站。');
+localStorage.setItem("DATA_e_T300",'##From now on,<br>This stop is added to Rt 39 (Praca Ferreira Amaral bound)');localStorage.setItem("DATA_T300",'##即日起<br>路線39前往亞馬喇前地方向新增停靠本站。');
+localStorage.setItem("DATA_e_T403",'##From now on,<br>This stop is added to Rt 39 (Edf. do Lago bound)');localStorage.setItem("DATA_T403",'##即日起<br>路線39前往湖畔大廈方向新增停靠本站。');
+localStorage.setItem("DATA_e_T304",'##From now on,<br>This stop is added to Rt 39 (Edf. do Lago bound)');localStorage.setItem("DATA_T304",'##即日起<br>路線39前往湖畔大廈方向新增停靠本站。');
+localStorage.setItem("DATA_e_T309",'##From now on,<br>This stop is added to Rt 39 (Edf. do Lago bound)');localStorage.setItem("DATA_T309",'##即日起<br>路線39前往湖畔大廈方向新增停靠本站。');
+localStorage.setItem("DATA_e_r52x",'##From now on,<br>"PONTE DE SAI VAN/AV. DO OCEANO" stop (located under footbridge between Ocean Garden Bldg and Bikeway) is added to this route and also "Heavy-duty Vehicles Driving and Examine Centre".');localStorage.setItem("DATA_r52x",'##即日起<br>本線新增停靠位於海洋花園行人天橋下的「西灣大橋/海洋大馬路」站及「重型車輛駕考中心」站。');
+localStorage.setItem("DATA_e_r52y",'##From now on,<br>"JARDINS DO OCEANO/ROSE COURT" stop (located under footbridge between Ocean Garden Bldg and Bikeway) is added to this route and also "Rotunda da Harmonia".');localStorage.setItem("DATA_r52y",'##即日起<br>本線新增停靠位於海洋花園行人天橋下的「海洋花園/玫瑰苑」站及「和諧圓形地」站。');
+localStorage.setItem("DATA_e_rMT5x",'##From now on,<br>"Praca Ferreira Amaral" and "Hotel President" stop are added to this route (NAPE bound).');localStorage.setItem("DATA_rMT5x",'##即日起<br>本線往皇朝新增停靠「亞馬喇前地」站及「總統酒店」站。');
+localStorage.setItem("DATA_e_M172F",'##From now on,<br>This stop is added to Rt MT5 (NAPE bound)');localStorage.setItem("DATA_M172F",'##即日起<br>MT5往皇朝新增停靠本站。');
+localStorage.setItem("DATA_e_M159",'##From now on,<br>This stop is added to Rt MT5 (NAPE bound)');localStorage.setItem("DATA_M159",'##即日起<br>MT5往皇朝新增停靠本站。');
+localStorage.setItem("DATA_e_T426",'##From now on,<br>This stop is cancelled.');localStorage.setItem("DATA_T426",'##即日起<br>本站停用。');
+localStorage.setItem("DATA_e_T427",'##From now on,<br>This stop is cancelled.');localStorage.setItem("DATA_T427",'##即日起<br>本站停用。');
+localStorage.setItem("DATA_e_r55x",'##From now on,<br>"Heavy-duty Vehicles Driving and Examine Centre" stop is added to Barra bound of this rt. Moreover, "Rotunda da Harmonia" stop is added to Seac Pai Van bound.');localStorage.setItem("DATA_r55x",'##即日起<br>本線往媽閣新增停靠「重型車輛駕考中心」站；往石排灣新增停靠「和諧圓形地」站。');
+localStorage.setItem("DATA_e_C682",'##From now on,<br>This stop is added to Rt 52 and Rt 55 (Seac Pai Van bound). Rt 21A skips this stop. This stop is moved to Av. Marginal Flor de Lotus (Near roundabout).');localStorage.setItem("DATA_C682",'##即日起<br>路線52及55往石排灣方向新增停靠本站，本站遷至蓮花海濱大馬路(近圓形地)，路線21A不停靠本站。');
+localStorage.setItem("DATA_e_C683",'##From now on,<br>This stop is added to Rt 52 (P.F.Amaral bound) and Rt 55 (Barra bound). Rt 21A skips this stop. This stop is moved to Av. Marginal Flor de Lotus (Near roundabout).');localStorage.setItem("DATA_C683",'##即日起<br>路線52及55往澳門方向新增停靠本站，本站遷至蓮花海濱大馬路(近圓形地)，路線21A不停靠本站。');
+localStorage.setItem("DATA_e_r21Ax",'##From now on,<br>"Heavy-duty Vehicles Driving and Examine Centre" stop is skipped.');localStorage.setItem("DATA_r21Ax",'##即日起<br>本線不停靠「和諧圓形地」站。');
+localStorage.setItem("DATA_e_r21Ay",'##From now on,<br>"Rotunda da Harmonia" stop is skipped.');localStorage.setItem("DATA_r21Ay",'##即日起<br>本線不停靠「重型車輛駕考中心」站。');
+
+
+
+
+
 
 if((vers!="v70")){
     var link = '<a ontouchstart="localStorage.setItem(\'getgetmacau\',\'\');localStorage.setItem(\'udmacau\',\'\');localStorage.setItem(\'udmacau_e\',\'\');window.location.href=\'https://play.google.com/store/apps/details?id=com.locmacau&apk\'" class="upd-icon">';
