353339b13e88405ab9c1e45a1cb0991c5f8d15fd
kcho <hellokcho@gmail.com>
2016-05-07 17:21:49 +0000

    updated

--- script.js
+++ script.js
@@ -20,11 +20,11 @@ localStorage.setItem("DATA_e_T347",'##From now on,<br>This stop is moved to Rua 
 
 
 
-
+/*
 if((vers!="v70")){
     var link = '<a ontouchstart="localStorage.setItem(\'getgetmacau\',\'\');localStorage.setItem(\'udmacau\',\'\');localStorage.setItem(\'udmacau_e\',\'\');window.location.href=\'https://play.google.com/store/apps/details?id=com.locmacau&apk\'" class="upd-icon">';
 
     localStorage.setItem("udmacau",link+"<b>澳門出行易 V4.3.2</b><u>點到點搜尋通宵路線</u><div>檢視</div></a>");
     localStorage.setItem("udmacau_e",link+"<b>Macau EasyGo V4.3.2</b><u>Overnight Rt Option for Point-to-point Rt Search</u><div>VIEW</div></a>");
     window.location.reload();
-}
+}*/
