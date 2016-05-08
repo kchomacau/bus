e1e4a2c9fba16f4c914473ce39cb4b801e42061e
kcho <hellokcho@gmail.com>
2016-05-08 06:10:32 +0000

    updated

--- script.js
+++ script.js
@@ -4,23 +4,6 @@ localStorage.removeItem("udmacau");
 localStorage.removeItem("udmacau_e");
 
 
-localStorage.setItem("DATA_e_r34y",'##From now on,<br>Rt 34 skips "Rua de Aveiro", "Hou Kong Middle School Taipa" and "Rua Evora/Flower City" stop. You may use "Nam Sun Garden Bldg.", "Va Pou Garden Bldg." or "Garden of Flower City" stop instead.');localStorage.setItem("DATA_r34y",'##即日起<br>本線不停靠「亞威羅街」、「氹仔濠江中學」及「埃武拉街/花城」站，您可使用「南新花園」、「華寶花園」或「花城公園」站上落。');
-localStorage.setItem("DATA_e_T348",'##From now on,<br>This stop is cancelled. You may use "Va Pou Garden Bldg." or "Garden of Flower City" stop instead.');localStorage.setItem("DATA_T348",'##即日起<br>本站停用，您可使用「華寶花園」或「花城公園」站上落。');
-localStorage.setItem("DATA_e_T349",'##From now on,<br>This stop is cancelled. You may use "Kingsville" stop instead.');localStorage.setItem("DATA_T349",'##即日起<br>本站停用，您可使用「雍景灣」站上落。');
-localStorage.setItem("DATA_e_T340",'##From now on,<br>Rt 34 skips this stop. You may use "Nam Sun Garden Bldg." stop instead.');localStorage.setItem("DATA_T340",'##即日起<br>路線34不停靠本站，您可使用「南新花園」站上落。');
-localStorage.setItem("DATA_e_r4x",'##From now on,<br>"Van Sion Son Chun" temp. stop is added after "Lam Mau/Macau Yacht Club".');localStorage.setItem("DATA_r4x",'##即日起<br>本線在停靠「林茂/澳門遊艇會」站後，新增停靠「運順新邨」臨時站，您可使用該站上落。');
-localStorage.setItem("DATA_e_M184",'##From now on,<br>This stop is moved forwards 100m, opposites to McDonald\'s.');localStorage.setItem("DATA_M184",'##即日起<br>本站往媽閣方向遷移至小販巷口。');
-localStorage.setItem("DATA_e_T426",'##From now on,<br>This stop is cancelled.');localStorage.setItem("DATA_T426",'##即日起<br>本站停用。');
-localStorage.setItem("DATA_e_T427",'##From now on,<br>This stop is cancelled.');localStorage.setItem("DATA_T427",'##即日起<br>本站停用。');
-localStorage.setItem("DATA_e_C682",'##From now on,<br>Rt 21A skips this stop.');localStorage.setItem("DATA_C682",'##即日起<br>路線21A不停靠本站。');
-localStorage.setItem("DATA_e_C683",'##From now on,<br>Rt 21A skips this stop.');localStorage.setItem("DATA_C683",'##即日起<br>路線21A不停靠本站。');
-localStorage.setItem("DATA_e_r21Ax",'##From now on,<br>"Heavy-duty Vehicles Driving and Examine Centre" stop is skipped.');localStorage.setItem("DATA_r21Ax",'##即日起<br>本線不停靠「和諧圓形地」站。');
-localStorage.setItem("DATA_e_r21Ay",'##From now on,<br>"Rotunda da Harmonia" stop is skipped.');localStorage.setItem("DATA_r21Ay",'##即日起<br>本線不停靠「重型車輛駕考中心」站。');
-localStorage.setItem("DATA_e_T347",'##From now on,<br>This stop is moved to Rua do Desporto, opposite to Macau Olympic Aquatic Centre.');localStorage.setItem("DATA_T347",'##即日起<br>本站臨時遷移至體育路 奧林匹克游泳館對面。');
-
-
-
-
 if((vers!="v71")){
     var link = '<a ontouchstart="localStorage.setItem(\'getgetmacau\',\'\');localStorage.setItem(\'udmacau\',\'\');localStorage.setItem(\'udmacau_e\',\'\');window.location.href=\'https://play.google.com/store/apps/details?id=com.locmacau&apk\'" class="upd-icon">';
 
