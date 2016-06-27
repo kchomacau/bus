f859cfe6e5b246884e1d4367328f6d25830323b2
kcho <hellokcho@gmail.com>
2016-06-27 02:00:26 +0000

    updated

--- realtimebus_v74.js
+++ realtimebus_v74.js
@@ -2,6 +2,9 @@ var url_first = "http://202.175.87.15:7012/macauweb/bootstrap/lineDiagram.html?r
 var url_second = "&dir=";
 var url_last = "&lang=zh-tw";
 
+var div_wrap_default_class_2 = div_wrap_default_class;
+copy_dsat.style.display="none";
+
 function stopIt(){
     stop_array={by_kc:[]}
 }
