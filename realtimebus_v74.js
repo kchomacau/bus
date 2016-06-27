e0c32e0284d7c2c1219ae16974f4ce667b10df66
kcho <hellokcho@gmail.com>
2016-06-27 02:03:09 +0000

    updated

--- realtimebus_v74.js
+++ realtimebus_v74.js
@@ -1,14 +1,3 @@
 var url_first = "http://202.175.87.15:7012/macauweb/bootstrap/lineDiagram.html?routeName=";
 var url_second = "&dir=";
 var url_last = "&lang=zh-tw";
-
-div_wrap_default_class_2 = div_wrap_default_class;
-copy_dsat.style.display="none";
-
-function stopIt(){
-    stop_array={by_kc:[]}
-}
-window.setTimeout(stopIt,300);
-window.setTimeout(stopIt,400);
-window.setTimeout(stopIt,450);
-window.setTimeout(stopIt,500);
