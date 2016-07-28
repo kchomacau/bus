63bfe39fbb6103666d88a3f488d4cb9f2d3ef609
kcho <hellokcho@gmail.com>
2016-07-28 03:56:58 +0000

    updated

--- realtimebus_v74.js
+++ realtimebus_v74.js
@@ -1,3 +1,4 @@
 url_first = "http://202.175.87.15:7012/macauweb/bootstrap/lineDiagram.html?routeName=";
 url_second = "&dir=";
 url_last = "&lang=zh-tw";
+avai_rt_array = ["1","2","3","4","5","7","10","11","26","33"];
