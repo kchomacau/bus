1d94c2eefe5a7ef5d6fea099aad635f5ca3e6f43
kcho <hellokcho@gmail.com>
2016-09-04 05:17:11 +0000

    updated-test

--- script.js
+++ script.js
@@ -3,7 +3,22 @@ passedt();
 localStorage.removeItem("udmacau");
 localStorage.removeItem("udmacau_e");
 
+var katcheong_tc_map = '改為停靠設於澳門福建同鄉總會前的「吉祥樓」站，<a href="javascript:full(\'22.212132,113.549705\',\'吉祥樓臨時站\')" style="color:#0097ff">請按此處查看該站的離線地圖</a>。';
+var katcheong_en_map = 'Use the temp stop instead. <a href="javascript:full_e(\'22.212132,113.549705\',\'Edf. Kat Cheong Temp. Stop\')" style="color:#0097ff">Click here to view offline map of the temp stop.</a>';
 
+var katcheong_tc = '即日起<br>路線10、10X及27不停靠本站，'+katcheong_tc_map;
+var katcheong_en = 'From now on<br>Rt 10, 10X and 27 skip this stop. '+katcheong_en_map;
+
+localStorage.setItem('DATA_M225','##'+katcheong_tc);
+localStorage.setItem('DATA_e_M225','##'+katcheong_en);
+localStorage.setItem('DATA_M7','##'+katcheong_tc);
+localStorage.setItem('DATA_e_M7','##'+katcheong_en);
+
+localStorage.setItem('DATA_M27','##即日起<br>路線10及10X不停靠本站，'+katcheong_tc_map);
+localStorage.setItem('DATA_e_M27','##From now on<br>Rt 10 and 10X skip this stop. '+katcheong_en_map);
+
+localStorage.setItem('DATA_M229','##即日起<br>路線27不停靠本站，可使用「拱形馬路/蓮峰廟」站。');
+localStorage.setItem('DATA_e_M229','##From now on<br>Rt 27 skip this stop. Use "Est. Arco/Lin Fung Temple" stop instead.');
 
 
 if((vers!="v79")){
