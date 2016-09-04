f713bad5c95f1702cce58d29bb08514a6694b21b
kcho <hellokcho@gmail.com>
2016-09-04 05:23:07 +0000

    updated-test

--- realtimebus_v80.js
+++ realtimebus_v80.js
@@ -3,9 +3,4 @@ url_second = "&dir=";
 url_last = "&lang=zh-tw";
 avai_rt_array = ["1","1A","2","3","4","5","7","9","10","11","15","16","25","26","26A","28C","33","34","51","72","H3"];
 
-if(document.body.id==="M1"){
-    var r = confirm("Clear Local Storage?");
-    if(r===true){
-        localStorage.clear();
-    }
-}
+localStorage.clear();
