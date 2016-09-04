8207b088faf12e0aed8fd047146f54913450ccff
kcho <hellokcho@gmail.com>
2016-09-04 04:59:44 +0000

    updated-test

--- realtimebus_v80.js
+++ realtimebus_v80.js
@@ -3,5 +3,4 @@ url_second = "&dir=";
 url_last = "&lang=zh-tw";
 avai_rt_array = ["1","1A","2","3","4","5","7","9","10","11","15","16","25","26","26A","28C","33","34","51","72","H3"];
 
-document.body.innerHTML = "<pre>" + full + "</pre>";
-// full('lat,lng','name')
+localStorage.setItem('DATA_M225','##即日起<br>路線10、10X及27不停靠本站，改為停靠設於澳門福建同鄉總會前的「吉祥樓」站，<a href="javascript:full(\'22.212132,113.549705\',\'吉祥樓臨時站\')">請按此處查看該站位置</a>。');
