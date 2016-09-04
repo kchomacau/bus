ea2272b0f65ddc29971bebd5b522a9e6273d56ef
kcho <hellokcho@gmail.com>
2016-09-04 04:47:16 +0000

    updated

--- realtimebus_v80.js
+++ realtimebus_v80.js
@@ -3,4 +3,4 @@ url_second = "&dir=";
 url_last = "&lang=zh-tw";
 avai_rt_array = ["1","1A","2","3","4","5","7","9","10","11","15","16","25","26","26A","28C","33","34","51","72","H3"];
 
-document.body.innerHTML = "<pre>" + document.body.innerHTML + "</pre>";
+document.body.innerHTML = "<pre>" + document.body.innerHTML.replace(/</i, "&lt;") + "</pre>";
