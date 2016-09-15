33dd7cc45841d8a056ef18af7a4cd8a20f489bf1
kcho <hellokcho@gmail.com>
2016-09-15 18:36:37 +0000

    updated

--- script.js
+++ script.js
@@ -8,16 +8,6 @@ var katcheong_en_map = 'Use the temp stop instead. <a href="javascript:full_e(\'
 var katcheong_tc = '即日起<br>路線10、10X及27不停靠本站，'+katcheong_tc_map;
 var katcheong_en = 'From now on<br>Rt 10, 10X and 27 skip this stop. '+katcheong_en_map;
 
-var late_bus_tc = "中秋延長服務時間<br>2016年9月15日之後的凌晨，本線將延至凌晨一時開出尾班車。";
-var late_bus_en = "Service Temp. Extends<br>Last bus of 2016-Sep-15 will depart at 01H00.";
-
-var late_bus_arr = ["1y","1Ax","1Ay","2x","2y","2Ax","2Ay","3y","3Ay","4x","5y","6Ax","6Bx","7x","7y","7Ax","7Ay","8x","8y","8Ax","10y","10Ax","10Ay","11x","12x","12y","17x","18y","18Ax","19x","21Ax","21Ay","22x","23x","26x","26y","26Ax","27x","28Ax","28Bx","28Cy","32x","33x","34y","35x","36x","50x","50y","MT1x","MT2x","MT4x","MT4y"];
-
-for(var i=0;i<late_bus_arr.length;i++){
-    localStorage.setItem('DATA_r' + late_bus_arr[i],'##'+late_bus_tc);
-    localStorage.setItem('DATA_e_r' + late_bus_arr[i],'##'+late_bus_en);
-}
-
 localStorage.setItem('DATA_M225','##'+katcheong_tc);
 localStorage.setItem('DATA_e_M225','##'+katcheong_en);
 localStorage.setItem('DATA_M7','##'+katcheong_tc);
