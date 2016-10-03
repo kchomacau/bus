2460c82b8148a57541a89e1d2901b60a754e2111
kcho <hellokcho@gmail.com>
2016-10-03 08:15:01 +0000

    updated

--- script.js
+++ script.js
@@ -3,13 +3,10 @@ passedt();
 localStorage.removeItem("udmacau");
 localStorage.removeItem("udmacau_e");
 
-localStorage.setItem('DATA_T307','##即日起<br>本站停用。');
-localStorage.setItem('DATA_e_T307','##From now on<br>This stop is suspended.');
-
-if((vers!="v80")){
+if((vers!="v81")){
     var link = '<a ontouchstart="localStorage.setItem(\'getgetmacau\',\'\');localStorage.setItem(\'udmacau\',\'\');localStorage.setItem(\'udmacau_e\',\'\');window.location.href=\'https://play.google.com/store/apps/details?id=com.locmacau&apk\'" class="upd-icon">';
-    localStorage.setItem("udmacau",link+"<b>澳門出行易 V4.4.0</b><u>更新離島區路線</u><div>檢視</div></a>");
-    localStorage.setItem("udmacau_e",link+"<b>Macau EasyGo V4.4.0</b><u>Update Island Routes</u><div>VIEW</div></a>");
+    localStorage.setItem("udmacau",link+"<b>澳門出行易 V4.4.1</b><u>加入H2路線</u><div>檢視</div></a>");
+    localStorage.setItem("udmacau_e",link+"<b>Macau EasyGo V4.4.1</b><u>Add Rt. H2</u><div>VIEW</div></a>");
     window.location.reload();
 }
 
