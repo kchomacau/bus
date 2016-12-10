64f0514a074b7a4e196211b19bb9c6fefbce28b9
kcho <hellokcho@gmail.com>
2016-12-10 08:12:03 +0000

    updated

--- realtimebus_v83.js
+++ realtimebus_v83.js
@@ -2,6 +2,7 @@ url_first = "http://202.175.87.15:7012/macauweb/bootstrap/lineDiagram.html?route
 url_second = "&dir=";
 url_last = "&lang=zh-tw";
 avai_rt_array = ["1","1A","2","2A","3","3A","3X","4","5","5X","6A","6B","7","7A","8","8A","9","9A","10","10A","10B","10X","11","12","15","16","17","18","18A","19","21A","22","23","25","25X","26","26A","27","28A","28B","28BX","28C","30","30X","32","33","34","35","36","37","39","50","50X","51","51A","52","55","56","59","71","72","73","AP1","AP1X","H1","H2","H3","MT1","MT2","MT3","MT4","MT5","N1A","N1B","N2","N3","N4","N5","N6"];
+show_plate = false;
 
 function show(i){
 	window.clearInterval(interval);
