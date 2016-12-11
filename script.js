37af853b941a74c911003e1d397be0882a19f106
kcho <hellokcho@gmail.com>
2016-12-11 17:28:45 +0000

    updated

--- script.js
+++ script.js
@@ -14,30 +14,41 @@ if(vers!="v83"){
 	window.location.reload();
 }
 
-			// for old version, important changing.
-			if(vers!="v83"){
-
+var begin_u = '<strong style="font-weight:normal;text-decoration:underline">';
+var end_u = '</strong>';
 
 var arr = [
-	
+	{
+		r:["rN5x"],
+		e:"From now on, \""+begin_u+"Airport Cargo Terminal"+end_u+"\" stop is added after \"Rotunda do Aeroporto/Wai Long\".",
+		c:"即日起<br>本線在停靠「機場圓形地/偉龍」站後，新增停靠「"+begin_u+"機場貨運站"+end_u+"」。"
+	},
+	{
+		r:["T417"],
+		e:"From now on, "+begin_u+"Rt N5"+end_u+" uses this stop for "+begin_u+"Av. Vale das Borboletas"+end_u+" bound.",
+		c:"即日起<br>"+begin_u+"N5線往蝴蝶谷大馬路"+end_u+"停靠本站。"
+	}
+];
+
+			// for old version, important changing.
+			if(vers!="v83"){
+arr.push(
 	{
 		r:["r39Xx"],
-		e:"Service changed<br>Bus departs from Praca Ferreira Amaral. Edf Lago became a way stop of this route.",
-		c:"服務調整<br>改為由亞馬喇前地開出；湖畔大廈改為中途站。"
+		e:"Service changed<br>Bus departs from "+begin_u+"Praca Ferreira Amaral"+end_u+". Edf Lago became a way stop of this route.",
+		c:"服務調整<br>改為由"+begin_u+"亞馬喇前地"+end_u+"開出；湖畔大廈改為中途站。"
 	},
 	{
 		r:["rMT5x"],
-		e:"Timetable changed<br><font color=crimson>No service on Sundays and public holidays.</font></strong>.",
-		c:"班次調整<br><font color=crimson>星期日及公眾假期停駛。</font></strong>"
+		e:"Timetable changed<br><strong><font color=crimson>No service on Sundays and public holidays.</font></strong>.",
+		c:"班次調整<br><strong><font color=crimson>星期日及公眾假期停駛。</font></strong>"
 	},
 	{
 		r:["M74"],
-		e:"Important Notice<br>Due to route adjustment, Rt 7, 7A and 8 using this stop are going to <font color=crimson>Hospital Kiang Wu</font> (which is the reverse trip of the info listed below). For Rt 7A or 8 to Praca Ferreira Amaral, go to \"Tap Seac Multisport Pavilion\" stop. For Rt 7 to San Malo, go to \"Tap Seac Plaza\" stop.",
-		c:"重要通知<br>由於路線調整，停靠本站的 7、7A及8路線是前往<font color=crimson>鏡湖醫院</font>的（即與下方列出的路線方向剛好相反）。乘搭7A及8路線前往亞馬喇前地，請步行至「塔石體育館」站乘搭；乘搭7路線往新馬路，請步行至「塔石廣場」站乘搭。"
+		e:"Important Notice<br>Due to route adjustment, Rt 7, 7A and 8 using this stop are going to <strong><font color=crimson>Hospital Kiang Wu</font></strong> (which is the reverse trip of the info listed below). For Rt 7A or 8 to Praca Ferreira Amaral, go to \""+begin_u+"Tap Seac Multisport Pavilion"+end_u+"\" stop. For Rt 7 to San Malo, go to \""+begin_u+"Tap Seac Plaza"+end_u+"\" stop.",
+		c:"重要通知<br>由於路線調整，停靠本站的 7、7A及8路線是前往<strong><font color=crimson>鏡湖醫院</font></strong>的（即與下方列出的路線方向剛好相反）。乘搭7A及8路線前往亞馬喇前地，請步行至「"+begin_u+"塔石體育館"+end_u+"」站乘搭；乘搭7路線往新馬路，請步行至「"+begin_u+"塔石廣場"+end_u+"」站乘搭。"
 	}
-
-];
-for(var i=0;i<arr.length;i++){for(var j=0;j<arr[i].r.length;j++){localStorage.setItem("DATA_e_"+arr[i].r[j], '##'+arr[i].e);localStorage.setItem("DATA_" +arr[i].r[j], '##'+arr[i].c);}}
-
-
+);
 			}
+
+for(var i=0;i<arr.length;i++){for(var j=0;j<arr[i].r.length;j++){localStorage.setItem("DATA_e_"+arr[i].r[j], '##'+arr[i].e);localStorage.setItem("DATA_" +arr[i].r[j], '##'+arr[i].c);}}
