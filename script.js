d2d9863ad8741a83d804319a35af14cf42c7ec53
kcho <hellokcho@gmail.com>
2016-12-24 09:21:27 +0000

    updated

--- script.js
+++ script.js
@@ -1,56 +1,65 @@
-passedt();localStorage.removeItem("udmacau");localStorage.removeItem("udmacau_e");
+passedt();localStorage.removeItem("udmacau");localStorage.removeItem("udmacau_e");var ver = parseInt(vers.substr(1));
 
-var ver = parseInt(vers.substr(1));
 var cur_ver = 85;
 
-if(ver < cur_ver){
+	if(ver < cur_ver){
 
-	var tmp_js = {
-		ver: "4.4.5",
-		chi: "更新MT2路線",
-		eng: "Update Rt MT2"
-	};
+		var tmp_js = {
+			ver: "4.4.5",
+			chi: "更新MT2路線",
+			eng: "Update Rt MT2"
+		};
 
-	var link = '<a ontouchstart="localStorage.setItem(\'getgetmacau\',\'\');localStorage.setItem(\'udmacau\',\'\');localStorage.setItem(\'udmacau_e\',\'\');window.location.href=\'https://play.google.com/store/apps/details?id=com.locmacau&apk\'" class="upd-icon">';
-	localStorage.setItem("udmacau",link+"<b>澳門出行易 V"+tmp_js.ver+"</b><u>"+tmp_js.chi+"</u><div>檢視</div></a>");
-	localStorage.setItem("udmacau_e",link+"<b>Macau EasyGo V"+tmp_js.ver+"</b><u>"+tmp_js.eng+"</u><div>VIEW</div></a>");
-	window.location.reload();
-}
+		var link = '<a ontouchstart="localStorage.setItem(\'getgetmacau\',\'\');localStorage.setItem(\'udmacau\',\'\');localStorage.setItem(\'udmacau_e\',\'\');window.location.href=\'https://play.google.com/store/apps/details?id=com.locmacau&apk\'" class="upd-icon">';
+		var end_link = '</a>';
+		localStorage.setItem("udmacau",link+"<b>澳門出行易 V"+tmp_js.ver+"</b><u>"+tmp_js.chi+"</u><div>檢視</div>" + end_link);
+		localStorage.setItem("udmacau_e",link+"<b>Macau EasyGo V"+tmp_js.ver+"</b><u>"+tmp_js.eng+"</u><div>VIEW</div>" + end_link);
+		window.location.reload();
+	}
 
 var begin_u = '<strong style="font-weight:normal;text-decoration:underline">';
 var end_u = '</strong>';
 
 var arr = [
+
 ];
 
-			// for old version, important changing.
-			if(ver < 84){
-arr.push(
-	{
-		r:["rMT2x"],
-		e:"Service changed<br>This route will return at Hotel Taipa Square. For Airport, take Rt MT1.",
-		c:"服務調整<br>本線將於駿景酒店折返，前往機場請選乘MT1路線。"
-	}
-);
+	arr.push(
+		{
+			r:["r1y","r1Ax","r1Ay","r2x","r2y","r2Ax","r2Ay","r3y","r3Ay","r4x","r5y","r6Ax","r7x","r7y","r7Ax","r7Ay","r8x","r8y","r8Ax","r10y","r10Ax","r10Ay","r11x","r12x","r12y","r17x","r18y","r18Ax","r19x","r21Ax","r21Ay","r22x","r23x","r26x","r26y","r26Ax","r27x","r28Ax","r28Bx","r28Cy","r32x","r33x","r34y","r35x","r36x","r50x","r50y","rMT1x","rMT2x","rMT4x","rMT4y"],
+			e:"Service Extend on Christmas Eve<br>The last bus of this route will depart at 25H00 (i.e., 1 am Dec 25).",
+			c:"平安夜尾班服務<br>本線尾班車將延至25H00（凌晨一時）。"
+		}
+	);
+
+	// for old version, important changing.
+	if(ver < 84){
+		arr.push(
+			{
+				r:["rMT2x"],
+				e:"Service changed<br>This route will return at Hotel Taipa Square. For Airport, take Rt MT1.",
+				c:"服務調整<br>本線將於駿景酒店折返，前往機場請選乘MT1路線。"
 			}
-			if(ver < 83){
-arr.push(
-	{
-		r:["r39x"],
-		e:"Service changed<br>Bus departs from "+begin_u+"Praca Ferreira Amaral"+end_u+". Edf Lago became a way stop of this route.",
-		c:"服務調整<br>改為由"+begin_u+"亞馬喇前地"+end_u+"開出；湖畔大廈改為中途站。"
-	},
-	{
-		r:["rMT5x"],
-		e:"Timetable changed<br><strong><font color=crimson>No service on Sundays and public holidays.</font></strong>.",
-		c:"班次調整<br><strong><font color=crimson>星期日及公眾假期停駛。</font></strong>"
-	},
-	{
-		r:["M74"],
-		e:"Important Notice<br>Due to route adjustment, Rt 7, 7A and 8 using this stop are going to <strong><font color=crimson>Hospital Kiang Wu</font></strong> (which is the reverse trip of the info listed below). For Rt 7A or 8 to Praca Ferreira Amaral, go to \""+begin_u+"Tap Seac Multisport Pavilion"+end_u+"\" stop. For Rt 7 to San Malo, go to \""+begin_u+"Tap Seac Plaza"+end_u+"\" stop.",
-		c:"重要通知<br>由於路線調整，停靠本站的 7、7A及8路線是前往<strong><font color=crimson>鏡湖醫院</font></strong>的（即與下方列出的路線方向剛好相反）。乘搭7A及8路線前往亞馬喇前地，請步行至「"+begin_u+"塔石體育館"+end_u+"」站乘搭；乘搭7路線往新馬路，請步行至「"+begin_u+"塔石廣場"+end_u+"」站乘搭。"
+		);
 	}
-);
+	if(ver < 83){
+		arr.push(
+			{
+				r:["r39x"],
+				e:"Service changed<br>Bus departs from "+begin_u+"Praca Ferreira Amaral"+end_u+". Edf Lago became a way stop of this route.",
+				c:"服務調整<br>改為由"+begin_u+"亞馬喇前地"+end_u+"開出；湖畔大廈改為中途站。"
+			},
+			{
+				r:["rMT5x"],
+				e:"Timetable changed<br><strong><font color=crimson>No service on Sundays and public holidays.</font></strong>.",
+				c:"班次調整<br><strong><font color=crimson>星期日及公眾假期停駛。</font></strong>"
+			},
+			{
+				r:["M74"],
+				e:"Important Notice<br>Due to route adjustment, Rt 7, 7A and 8 using this stop are going to <strong><font color=crimson>Hospital Kiang Wu</font></strong> (which is the reverse trip of the info listed below). For Rt 7A or 8 to Praca Ferreira Amaral, go to \""+begin_u+"Tap Seac Multisport Pavilion"+end_u+"\" stop. For Rt 7 to San Malo, go to \""+begin_u+"Tap Seac Plaza"+end_u+"\" stop.",
+				c:"重要通知<br>由於路線調整，停靠本站的 7、7A及8路線是前往<strong><font color=crimson>鏡湖醫院</font></strong>的（即與下方列出的路線方向剛好相反）。乘搭7A及8路線前往亞馬喇前地，請步行至「"+begin_u+"塔石體育館"+end_u+"」站乘搭；乘搭7路線往新馬路，請步行至「"+begin_u+"塔石廣場"+end_u+"」站乘搭。"
 			}
+		);
+	}
 
 for(var i=0;i<arr.length;i++){for(var j=0;j<arr[i].r.length;j++){localStorage.setItem("DATA_e_"+arr[i].r[j], '##'+arr[i].e);localStorage.setItem("DATA_" +arr[i].r[j], '##'+arr[i].c);}}
