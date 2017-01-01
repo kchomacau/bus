911fa4abdb4e1465d79507ae38b2ab451c4304b4
kcho <hellokcho@gmail.com>
2017-01-01 08:09:01 +0000

    updated with wish

--- script.js
+++ script.js
@@ -5,28 +5,6 @@ var end_style = "</div>";
 var chineseWish = begin_style + '澳門出行易謹祝您新年快樂！' + end_style;
 var englishWish = begin_style + 'Macau EasyGo wishes you a happy new year!' + end_style;
 
-var cur_ver = 85;
-
-	if(ver < cur_ver){
-
-		var tmp_js = {
-			ver: "4.4.5",
-			chi: "更新MT2路線",
-			eng: "Update Rt MT2"
-		};
-
-		var link = '<a ontouchstart="localStorage.setItem(\'getgetmacau\',\'\');localStorage.setItem(\'udmacau\',\'\');localStorage.setItem(\'udmacau_e\',\'\');window.location.href=\'https://play.google.com/store/apps/details?id=com.locmacau&apk\'" class="upd-icon">';
-		var end_link = '</a>';
-		localStorage.setItem("udmacau",link+"<b>澳門出行易 V"+tmp_js.ver+"</b><u>"+tmp_js.chi+"</u><div>檢視</div>" + end_link + chineseWish);
-		localStorage.setItem("udmacau_e",link+"<b>Macau EasyGo V"+tmp_js.ver+"</b><u>"+tmp_js.eng+"</u><div>VIEW</div>" + end_link + englishWish);
-		window.location.reload();
-	}
-	else{
-	    localStorage.setItem("udmacau",chineseWish);
-	    localStorage.setItem("udmacau_e",englishWish);
-	    window.location.reload();
-	}
-
 var begin_u = '<strong style="font-weight:normal;text-decoration:underline">';
 var end_u = '</strong>';
 
@@ -79,3 +57,25 @@ var arr = [
 	}
 
 for(var i=0;i<arr.length;i++){for(var j=0;j<arr[i].r.length;j++){localStorage.setItem("DATA_e_"+arr[i].r[j], '##'+arr[i].e);localStorage.setItem("DATA_" +arr[i].r[j], '##'+arr[i].c);}}
+
+var cur_ver = 85;
+
+	if(ver < cur_ver){
+
+		var tmp_js = {
+			ver: "4.4.5",
+			chi: "更新MT2路線",
+			eng: "Update Rt MT2"
+		};
+
+		var link = '<a ontouchstart="localStorage.setItem(\'getgetmacau\',\'\');localStorage.setItem(\'udmacau\',\'\');localStorage.setItem(\'udmacau_e\',\'\');window.location.href=\'https://play.google.com/store/apps/details?id=com.locmacau&apk\'" class="upd-icon">';
+		var end_link = '</a>';
+		localStorage.setItem("udmacau",link+"<b>澳門出行易 V"+tmp_js.ver+"</b><u>"+tmp_js.chi+"</u><div>檢視</div>" + end_link + chineseWish);
+		localStorage.setItem("udmacau_e",link+"<b>Macau EasyGo V"+tmp_js.ver+"</b><u>"+tmp_js.eng+"</u><div>VIEW</div>" + end_link + englishWish);
+		window.location.reload();
+	}
+	else{
+	    localStorage.setItem("udmacau",chineseWish);
+	    localStorage.setItem("udmacau_e",englishWish);
+	    window.location.reload();
+	}
