2fcce96d79cc36716129cfc3dd2d170800376178
kcho <hellokcho@gmail.com>
2017-04-15 06:35:11 +0000

    oriental arch divert

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ var begin_u = '<strong style="font-weight:normal;text-decoration:underline">';
 var end_u = '</strong>';
 
 var arr = [
+	{
+		r:["M155"],
+		e:'18-27 Apr 2017<br>Due to roadwork, for aboarding (except Rt 5), you need to go "Garden of Comendador Ho Yin" stop.',
+		c:"18-27/4/2017<br>工程影響，除5路線以外，其餘路線需於「何賢公園」乘搭。"
+	}
 ];
 
 var cur_ver = 88;
@@ -14,8 +19,8 @@ var tmp_js = {
 };
 
 if(ver < cur_ver){
-	arr = [
 	
+	arr.push(
 		{
 			r:["r5x"],
 			e:"<strong>Diverted</strong><br>Skips Av. Coronel Mesquita. Go Horta e Costa for this route.",
@@ -26,7 +31,7 @@ if(ver < cur_ver){
 			e:"<strong>Diverted</strong><br>Skips San Malo <-> Barra. Take Rt 2 instead.",
 			c:"<strong>改道</strong><br>本線不再行經新馬路至媽閣，請改乘2路線。"
 		}
-	];
+	);
 }
 
 for(var i=0;i<arr.length;i++){for(var j=0;j<arr[i].r.length;j++){localStorage.setItem("DATA_e_"+arr[i].r[j], '##'+arr[i].e);localStorage.setItem("DATA_" +arr[i].r[j], '##'+arr[i].c);}}
