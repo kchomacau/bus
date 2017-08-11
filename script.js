9fcd28da38473e466b1491efa7e441ae7e47f586
kcho <hellokcho@gmail.com>
2017-08-11 02:24:55 +0000

    v 4.4.12B - recall - news

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 passedt();localStorage.removeItem("udmacau");localStorage.removeItem("udmacau_e");var ver = parseInt(vers.substr(1));
 
-var arr = [
-];
+if(ver < 95){
 
-var cur_ver = 94;
-var tmp_js = {
-	ver: "4.4.12B",
-	chi: "修復GPS地圖顯示",
-	eng: "Fix Bug: Map w/ GPS"
-};
+var begin_style = '<div style="font-size: 17px;line-height:110%;background: limegreen;display:block;clear:both;color: #222;padding: 12px;font-weight: bold;">';
+var end_style = "</div>";
+var chineseWish = begin_style + '最新通知(11/8 10:20 am)<br>最新修復版本(V4.4.12C)仍在審核中，請不必急於更新。' + end_style;
+var englishWish = begin_style + 'Latest News (11 Aug 10:20 am)<br>The latest bug fixing version (V4.4.12C) is under validation. You don\'t need to update now.' + end_style;
 
+		localStorage.setItem("udmacau",chineseWish);
+		localStorage.setItem("udmacau_e",englishWish);
+		window.location.reload();
+		
+}
