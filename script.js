f80f77b3bbe95800bd5813e2fba78870b8680ff3
kcho <hellokcho@gmail.com>
2017-11-14 14:37:17 +0000

    car race news

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 passedt();localStorage.removeItem("udmacau");localStorage.removeItem("udmacau_e");var ver = parseInt(vers.substr(1));
 
+var begin_u = '<div style="font-size: 15px;line-height:20px;background: crimson;display:block;clear:both;color: #fff;padding: 20px;font-weight: bold;">';
+var end_u = '</div>';
+
+chineseText = begin_u + '2017年11月16日至19日賽車封路期間，各大路線將有較大改動，請留意巴士站及車廂通告。' + end_u;
+englishText = begin_u + 'During car race (2017 Nov 16 - 19), bus routes will be diverted. Pls pay attention to notices posted inside the bus.' + end_u;
 
 var arr = [
 ];
@@ -11,18 +16,6 @@ var tmp_js = {
 	eng: "Update routes around Border Gate"
 };
 
-if(ver < 94){
-	
-	arr.push(
-{
-r:["r11x","r22x","r25Xx","r26y","r26Ax","r30x","r33x","r34x","r35x","r37x","rMT1x","rMT3x"],
-e:"Temp Diversion until Sep 2017:<br>Skip Taipa Village (Rua do Cunha). Install latest version for latest detail.",
-c:"臨時改道至2017年9月<br>不進入氹仔舊城區。請安裝最新版本以檢視最新停站資訊。"
-}
-	);
-	
-}
-
 if(ver < cur_ver){
 }
 
@@ -32,7 +25,14 @@ for(var i=0;i<arr.length;i++){for(var j=0;j<arr[i].r.length;j++){localStorage.se
 
 		var link = '<a ontouchstart="localStorage.setItem(\'getgetmacau\',\'\');localStorage.setItem(\'udmacau\',\'\');localStorage.setItem(\'udmacau_e\',\'\');window.location.href=\'https://play.google.com/store/apps/details?id=com.locmacau&apk\'" class="upd-icon">';
 		var end_link = '</a>';
-		localStorage.setItem("udmacau",link+"<b>澳門出行易 V"+tmp_js.ver+"</b><u>"+tmp_js.chi+"</u><div>檢視</div>" + end_link);
-		localStorage.setItem("udmacau_e",link+"<b>Macau EasyGo V"+tmp_js.ver+"</b><u>"+tmp_js.eng+"</u><div>VIEW</div>" + end_link);
+		localStorage.setItem("udmacau",chineseText+link+"<b>澳門出行易 V"+tmp_js.ver+"</b><u>"+tmp_js.chi+"</u><div>檢視</div>" + end_link);
+		localStorage.setItem("udmacau_e",englishText+link+"<b>Macau EasyGo V"+tmp_js.ver+"</b><u>"+tmp_js.eng+"</u><div>VIEW</div>" + end_link);
 		window.location.reload();
 	}
+	else{
+		
+	    localStorage.setItem("udmacau",chineseText);
+	    localStorage.setItem("udmacau_e",englishText);
+	    window.location.reload();
+	}
+	
